e872e4c88d8489571b00dd33be692dab33487168
scott <scottsuk0306@kaist.ac.kr>
2022-01-12 16:11:24 +0900

    PS practice 1.12

---
+++ ABC-Browns/233-c.py
@@ -0,0 +1,15 @@
+# Code by tatyam
+from itertools import *
+N, X = map(int, input().split())
+A = []
+for i in range(N):
+  L, *a = map(int, input().split())
+  A.append(a)
+ans = 0
+for i in product(*A):
+  mul = 1
+  for x in i:
+    mul *= x
+  if mul == X:
+    ans += 1
+print(ans)
